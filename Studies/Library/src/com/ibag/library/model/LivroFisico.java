ac833610e5a8467677f06384e0d7b54cdfc39eaa
Gabrielle Ulisses <82672178+gabi-ulisses@users.noreply.github.com>
2025-04-04 19:32:31 -0300

    Update LivroFisico.java

--- Studies/Library/src/com/ibag/library/model/LivroFisico.java
+++ Studies/Library/src/com/ibag/library/model/LivroFisico.java
@@ -3,7 +3,7 @@ package com.ibag.library.model;
 public class LivroFisico extends MaterialBibliografico{
 
     public LivroFisico(String localizacao, String titulo, String autores, int edicao, int ano, int quantidade) {
-            super(localizacao, titulo, autores, edicao, ano, quantidade);
+            super(localizacao, titulo, autores, edicao, ano, 5);
             
         }
     
